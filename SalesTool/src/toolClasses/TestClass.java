e5d21a7049d0c7460c7f038a6476eac83fefc932
DeePak <kaushik.sst@gmail.com>
2015-03-11 17:53:12 +0530

    tool commit1

---
+++ SalesTool/src/toolClasses/TestClass.java
@@ -0,0 +1,16 @@
+package toolClasses;
+
+public class TestClass {
+	
+	
+	public static void main(String[] args)
+	{
+		
+		ToolClass tc = new ToolClass();
+		tc.test();
+		
+		System.out.println("tool tested");
+		
+	}
+
+}
---
+++ SalesTool/src/toolClasses/ToolClass.java
@@ -0,0 +1,10 @@
+package toolClasses;
+
+public class ToolClass {
+
+	public void test()
+	{
+		System.out.println("tool created");
+	}
+	
+}
